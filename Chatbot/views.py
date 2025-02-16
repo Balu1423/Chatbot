037368f54eb574d13505752823aba5e080e36f10
Balu1423 <balajichavidi@gmail.com>
2025-02-16 09:39:42 +0530

    nltk downloaded

--- Chatbot/views.py
+++ Chatbot/views.py
@@ -732,10 +732,10 @@ emotion_responses = {
 
 
 # Ensure that NLTK knows where to find the data (you can change this path as needed)
-nltk.data.path.append("nltk_data")
+# nltk.data.path.append("nltk_data")
 
 # Download the vader_lexicon if it hasn't been downloaded already
-nltk.download('vader_lexicon', download_dir="nltk_data")
+# nltk.download('vader_lexicon', download_dir="nltk_data")
 
 # Function to preprocess text: Tokenize, remove stopwords, and stem
 def preprocess_text(text):
